7114dc97b3811f346d5b816d57b1160c938e25a0
Victoria-Gudoit <vgudojt@mail.ru>
2022-06-01 23:18:42 +0300

    init: start project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Switch, Route } from "react-router-dom"
+import { Items } from "./components/items/Items"
+import { Item } from "./components/item/Item"
+import { Header } from "./components/Header/Header"
+import { Main } from "./components/Main/Main"
+import React from "react";
+
+export const App = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
